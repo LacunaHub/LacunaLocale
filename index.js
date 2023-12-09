91a0f3a35caa6e37757514bc41e145227544b00e
Daniyar Kurmangaliyev <randomna79@gmail.com>
2023-12-09 11:39:46 +0600

    Include english and ukrainian translation to index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const English = require('./translations/English/messages.json')
 const Russian = require('./translations/Russian/messages.json')
+const Ukrainian = require('./translations/Ukrainian/messages.json')
 
 const messages = {
-  ru: Russian
+  en: English,
+  ru: Russian,
+  uk: Ukrainian
 }
 
 const languages = {
-  ru: { name: 'Русский', code: 'ru' }
+  en: { name: 'English', code: 'en' },
+  ru: { name: 'Русский', code: 'ru' },
+  uk: { name: 'Українська', code: 'uk' }
 }
 
 module.exports = {
