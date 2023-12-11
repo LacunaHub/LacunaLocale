f1cf2c0088db609c618ec8268a8a3f74dd6f6708
Daniyar Kurmangaliyev <randomna79@gmail.com>
2023-12-11 10:34:52 +0600

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 import Russian from './translations/Russian/messages.json'
 
 export const messages: { [key: string]: typeof Russian }
-export const languages: { [key: string]: { name: string; code: string } }
+export const languages: Array<{ name: string; code: string }>
