8432516a45a9ec2298a7c9ef17d95609443942de
Daniyar Kurmangaliyev <randomna79@gmail.com>
2024-09-10 02:07:33 +0500

    feat: include french language

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const English = require('./translations/English/messages.json')
 const Russian = require('./translations/Russian/messages.json')
 const Ukrainian = require('./translations/Ukrainian/messages.json')
 const Belarusian = require('./translations/Belarusian/messages.json')
+const French = require('./translations/French/messages.json')
 
 const messages = {
   en: English,
   ru: Russian,
   uk: Ukrainian,
-  be: Belarusian
+  be: Belarusian,
+  fr: French
 }
 
 const languages = [
   { name: 'English', code: 'en' },
   { name: 'Русский', code: 'ru' },
   { name: 'Українська', code: 'uk' },
-  { name: 'Беларуская', code: 'be' }
+  { name: 'Беларуская', code: 'be' },
+  { name: 'Français', code: 'fr' }
 ]
 
 module.exports = {
