dc1a2899d51ac06d2701270041de32f5d537533e
Daniyar Kurmangaliyev <randomna79@gmail.com>
2024-09-10 02:03:35 +0500

    feat: include belarusian language

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const English = require('./translations/English/messages.json')
 const Russian = require('./translations/Russian/messages.json')
 const Ukrainian = require('./translations/Ukrainian/messages.json')
+const Belarusian = require('./translations/Belarusian/messages.json')
 
 const messages = {
   en: English,
   ru: Russian,
-  uk: Ukrainian
+  uk: Ukrainian,
+  be: Belarusian
 }
 
 const languages = [
   { name: 'English', code: 'en' },
   { name: 'Русский', code: 'ru' },
-  { name: 'Українська', code: 'uk' }
+  { name: 'Українська', code: 'uk' },
+  { name: 'Беларуская', code: 'be' }
 ]
 
 module.exports = {
